fcd31bb7d59f04fb7c280d9763d6a2147efff09b
ingpungya-github <ingpungya@gmail.com>
2021-03-07 22:28:40 +0900

    코드 최적화,주석 작성 (1차,주석) ------ 3차까지 할꺼고 주석작성,코드 최적화 진행할거임
    (사실 내일함)

--- src/main/java/Event/Main/EventUseCode.java
+++ src/main/java/Event/Main/EventUseCode.java
@@ -1,22 +1,22 @@
+//패키지
 package Event.Main;
 
+//임포트
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class EventUseCode implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        //접속 메세지
         Bukkit.broadcastMessage(ChatColor.YELLOW + "안녕하세요!" + event.getPlayer().getName() + ChatColor.BLUE + "님!");
-        //플레이어 정보 가져오기
+        //접속 메세지 2
         event.setJoinMessage("Quick Mc was already running");
+        //플레이어 정보 표시로 최소한의 치트 예방
         int viewdis = event.getPlayer().getClientViewDistance();
         String viewdisst = Integer.toString(viewdis);
         String pclinetbr = event.getPlayer().getClientBrandName();
--- src/main/java/ingpungya/Main/Main.java
+++ src/main/java/ingpungya/Main/Main.java
@@ -1,5 +1,7 @@
+//패키지
 package ingpungya.Main;
 
+//임포트
 import Event.Main.EventUseCode;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,13 +11,16 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        //실행 메세지
         System.out.println(ChatColor.GREEN + "Don't Panic");
+        //이벤트 처리
         getServer().getPluginManager().registerEvents(new EventUseCode(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        //종료 메세지
         System.out.println(ChatColor.GREEN + "Panic Now");
     }
 }
